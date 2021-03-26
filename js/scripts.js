02a7698967e13425d67770c1f64c2503cfa81540
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-26 10:25:20 -0700

    add logic branches with placeholder actions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const languageScore = eggKind + residence + shout + affil + trait
+
 $(document).ready(function(event) {
   $("language-test").submit(function() {
     const eggKind = parseInt($("input:radio[name=egg-kind]:checked").val())
@@ -6,7 +8,13 @@ $(document).ready(function(event) {
     const affil = parseInt($("input:radio[name=affil]:checked").val())
     const trait = parseInt($("input:radio[name=trait]:checked").val())
 
-    
+    if (languageScore <= 40) {
+      // this happens
+    } else if (languageScore > 40 && languageScore < 60) {
+      // this happens
+    } else if (languageScore >= 60) {
+      // this happens
+    }
 
     event.preventDefault()
   })
