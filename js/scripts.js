4e95e3a37e6f2ff08b2f6ed4ba9ef6a35ad95d0d
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-26 10:59:06 -0700

    add branch code for each condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,49 @@
-const languageScore = eggKind + residence + shout + affil + trait
+// Business logic
+
+function languageScore(eggKindVal, residenceVal, shoutVal, affilVal, traitVal) {
+  return eggKindVal + residenceVal + shoutVal + affilVal + traitVal
+}
+
+const ruby = "Ruby"
+const react = "React"
+const swift = "Swift"
+
+// User experience logic
 
 $(document).ready(function(event) {
   $("language-test").submit(function() {
-    const eggKind = parseInt($("input:radio[name=egg-kind]:checked").val())
-    const residence = parseInt($("input:radio[name=residence]:checked").val())
-    const shout = parseInt($("input:radio[name=shout]:checked").val())
-    const affil = parseInt($("input:radio[name=affil]:checked").val())
-    const trait = parseInt($("input:radio[name=trait]:checked").val())
+    const eggKindVal = parseInt($("input:radio[name=egg-kind]:checked").val())
+    const residenceVal = parseInt($("input:radio[name=residence]:checked").val())
+    const shoutVal = parseInt($("input:radio[name=shout]:checked").val())
+    const affilVal = parseInt($("input:radio[name=affil]:checked").val())
+    const traitVal = parseInt($("input:radio[name=trait]:checked").val())
+    const languageScore = languageScore(eggKindVal, residenceVal, shoutVal, affilVal, traitVal)
+
+    const reasonWhy = ($("p#form-results").text("reason-why")
 
     if (languageScore <= 40) {
-      // this happens
+      // Ruby, this happens
+      
+      $("#form-score").text(languageScore)
+      $("#language-result").text(ruby)
+      $("#form-results").text(reasonWhy)
+
     } else if (languageScore > 40 && languageScore < 60) {
-      // this happens
+      // React, this happens
+
+      $("#form-score").text(languageScore)
+      $("#language-result").text(react)
+      $("#form-results").text(reasonWhy)
+
     } else if (languageScore >= 60) {
-      // this happens
+      // Swift, this happens
+
+      $("#form-score").text(languageScore)
+      $("#language-result").text(swift)
+      $("#form-results").text(reasonWhy)
     }
+    
+    $("#form-feedback").show()
 
     event.preventDefault()
   })
