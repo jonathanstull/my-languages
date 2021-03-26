d946b55858d1ae4987d62c6e5dc3274c2e39f981
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-26 13:31:11 -0700

    refactor code for best practices

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,7 @@ $(document).ready(function() {
     const traitVal = parseInt($("input:radio[name=trait]:checked").val())
     const languageScore = eggKindVal + residenceVal + shoutVal + affilVal + traitVal
 
-    if (languageScore <= 40) {
-      // Ruby, this happens
-      
+    if (languageScore <= 40) {      
       $("#form-score").text(languageScore)
       $(".language-result").text(messageRuby)
       $("#form-results").text(reasonWhy)
@@ -27,8 +25,6 @@ $(document).ready(function() {
       $("#ruby-resources").show()
 
     } else if (languageScore > 40 && languageScore < 60) {
-      // React, this happens
-
       $("#form-score").text(languageScore)
       $(".language-result").text(messageReact)
       $("#form-results").text(reasonWhy)
@@ -36,8 +32,6 @@ $(document).ready(function() {
       $("#react-resources").show()
 
     } else if (languageScore >= 60) {
-      // Swift, this happens
-
       $("#form-score").text(languageScore)
       $(".language-result").text(messageSwift)
       $("#form-results").text(reasonWhy)
@@ -45,8 +39,6 @@ $(document).ready(function() {
       $("#swift-resources").show()
     }
     
-    
-
     event.preventDefault()
   })
 })
